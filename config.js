b53bbd15783dca5afa4bde1acb213189b26e5f18
buivanloi <100933683+buivanloi@users.noreply.github.com>
2022-03-04 23:09:04 +0700

    Add files via upload
    hihi

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '8/3';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Bùi Lợi chúc mừng ngày 8-3';
+const titleCard = 'Tặng Vy';
+const contentCard = 'Dẫu hôm nay là ngày phụ nữ. Tuy nhiên 8-3 sẽ thật thiếu vắng nếu như không dành những lời chúc cho em. Chúc Vy sẽ luôn hạnh phúc từ nay cho trở về sau và đặc biệt hãy cười nhiều lên nhé cô gái';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'thuy-vy.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
